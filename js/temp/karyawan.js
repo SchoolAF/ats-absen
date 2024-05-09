5155673d10adc692f2a559d875270960c927f9f0
HeroBuxx <me@herobuxx.me>
2024-05-09 22:05:07 +0700

    ats: Fix URL absensi per username
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- js/temp/karyawan.js
+++ js/temp/karyawan.js
@@ -8,7 +8,7 @@ export function generateKaryawanTable(karyawan) {
             ${karyawan.username}
         </td>
         <td class="px-6 py-4">
-        <a href="/absensi.html?username=${karyawan.username}">Lihat</a>
+        <a href="absensi.html?username=${karyawan.username}">Lihat</a>
         </td>
     `;
 }
